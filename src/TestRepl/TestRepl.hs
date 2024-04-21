9d27d45c30f94c6ea7dec77452f785d3bec08756
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-04-21 11:40:50 +0900

    it works enough that I can print a pointer to ghci I guess

--- src/TestRepl/TestRepl.hs
+++ src/TestRepl/TestRepl.hs
@@ -3,8 +3,15 @@ module TestRepl.TestRepl (
     ) where
 
 import WLR.Types.Scene
+import WL.ServerCore (wl_display_create)
+import Foreign (Word32)
+import WLR.Types.XdgShell (wlr_xdg_shell_create)
 
 testScene :: IO ()
 testScene = do
+    wlDisplay <- wl_display_create
+    print wlDisplay
     wlrScene <- wlr_scene_create
     print wlrScene
+    wlrXdgShell <- wlr_xdg_shell_create wlDisplay (1 :: Word32)
+    print wlrXdgShell
